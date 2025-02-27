f6112623e49169db16d58f101ddd72a303eb9dad
katrijnvandeun <k.vandeun@uvt.nl>
2025-02-27 11:11:01 +0100

    add standard operation

--- R/arithmetic_helpers.R
+++ R/arithmetic_helpers.R
@@ -6,6 +6,8 @@ perform_arithmetic <- function(number1, number2, operation) {
     return(number1 - number2)
   } else if (operation == "multiply") {
     return(number1 * number2)
+  } else if (operation == "standard") {
+    return(0)
   }
 }
 
